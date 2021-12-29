923ffec59d499fb822d7adc5eddda11eba4655de
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2021-12-29 11:04:23 -0700

    cleaned up file structure- /client npm start now runs app

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Tomodachi
-
-## Table of Contents
-
-## Demo
--- develop/client/src/pages/Login.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Auth from '../utils/auth';
-
-// function logic here
-
-return (
-    <div>
-    </div>
-)
