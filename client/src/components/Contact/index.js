7ce8904387b606913d4d103958e79bf092914fc0
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2021-12-29 12:28:53 -0700

    removed client submodule

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css'
+
+function Contact() {
+    return (
+        <div
+        className=""
+        >
+        </div>
+    )
+}
+
+export default Contact
---
+++ client/src/components/Pet/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css'
+
+function Pet() {
+    return (
+        <div
+        className=""
+        >
+        </div>
+    )
+}
+
+export default Pet
