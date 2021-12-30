594a54fe7e771185037fe9482bbbd249e5217d14
Tank <johnson90.cm@gmail.com>
2021-12-30 01:13:14 -0700

    app style.css and pet style.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
 import './App.css';
 import Nav from './components/Nav';
+import Pet from './components/Pet';
 import { useState } from 'react';
 import { Rainfall, Volcano, Sunlight, Wind } from './components/Interactions'
 
 function App() {
 
-  const [ animation, changeAnimation ] = useState('calm');
-  
+  const [animation, changeAnimation] = useState('calm');
+
   const renderAnimation = () => {
-    switch(animation) {
+    switch (animation) {
       case 'rainfall':
         console.log('cool rainfall animation');
-        setTimeout(()=>{changeAnimation('calm')}, 1500)
+        setTimeout(() => { changeAnimation('calm') }, 1500)
         break;
       case 'sunlight':
         console.log('cool sunlight animation');
-        setTimeout(()=>{changeAnimation('calm')}, 1500)
+        setTimeout(() => { changeAnimation('calm') }, 1500)
         break;
       case 'volcanoes':
         console.log('cool volcanoes animation');
-        setTimeout(()=>{changeAnimation('calm')}, 1500)
+        setTimeout(() => { changeAnimation('calm') }, 1500)
         break;
       case 'wind':
         console.log('cool wind animation');
-        setTimeout(()=>{changeAnimation('calm')}, 1500)
+        setTimeout(() => { changeAnimation('calm') }, 1500)
         break;
       default:
         console.log('cool calm animation')
@@ -31,22 +32,31 @@ function App() {
     }
   }
 
-    return (
-      <div className="App">
+  return (
+    <div className="wrapper">
+      <div className='container'>
         <Nav />
         <h1>
-          Welcome to Tomodachi
+          Tomodachi
         </h1>
+        <ul>
+          <li>Stat 1</li>
+          <li>Stat 2</li>
+          <li>Stat 3</li>
+          <li>Stat 4</li>
+        </ul>
+        <Pet />
         <div>
           {renderAnimation()}
         </div>
-        <Rainfall changeAnimation={changeAnimation} />
-        <Volcano changeAnimation={changeAnimation} />
-        <Sunlight changeAnimation={changeAnimation} />
-        <Wind changeAnimation={changeAnimation} />
+          <Rainfall changeAnimation={changeAnimation} />
+          <Volcano changeAnimation={changeAnimation} />
+          <Sunlight changeAnimation={changeAnimation} />
+          <Wind changeAnimation={changeAnimation} />
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 export default App;
