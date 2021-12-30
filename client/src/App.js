ce3bb1d16b6abac45e79e97cc4fdd9fbdcd11ce9
DerVerruckte <michael.heinhold@gmail.com>
2021-12-29 17:15:04 -0700

    added an animation state and logic to handle changes and update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
 import Nav from './components/Nav'
 import Rainfall from './components/Interactions'
+import { useState } from 'react';
 
 function App() {
+
+  const [ animation, changeAnimation ] = useState('calm');
+
+  const renderAnimation = () => {
+    switch(animation) {
+      case 'rainfall':
+        console.log('cool rainfall animation');
+        setTimeout(()=>{changeAnimation('calm')}, 1500)
+        break;
+      case 'sunlight':
+        break;
+      case 'volcanoes':
+        break;
+      case 'wind':
+        break;
+      default:
+        console.log('cool calm animation')
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <Nav />
       <h1>
         Welcome to Tomodachi
       </h1>
-      <Rainfall />
+      <div >
+        {renderAnimation()}
+      </div>
+      <Rainfall changeAnimation={changeAnimation} />
     </div>
   );
 }
