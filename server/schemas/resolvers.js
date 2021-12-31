7f6e6df35cf5512f2721cc66d1d59d919631e103
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-30 19:49:46 -0700

    minor tweak -__v

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,9 +6,8 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user.id }).select(
-          "__v -password"
-        );
+        const userData = await User.findOne({ _id: context.user.id })
+        .select("-__v -passwordz");
 
         return userData;
       }
@@ -16,11 +15,13 @@ const resolvers = {
     },
     // get all users
     users: async () => {
-      return User.find().select("__v -password");
+      return User
+      .find()
+      .select("-__v -password");
     },
     // get user by username
     user: async (parent, { username }) => {
-      return User.findOne({ username }).select("__v -password");
+      return User.findOne({ username }).select("-__v -password");
     },
   },
   Mutation: {
