356c5c4f9705ca2a8cd0896a65855779aa29794d
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-30 20:57:01 -0700

    updated index

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,10 @@
 const User = require('./User');
+const Planet = require('./Planet');
+const Interaction = require('./Interaction');
 
-module.exports = { User };
+module.exports = { User, Planet, Interaction };
+
+// add helper function here
+// takes in spheres and returns an array of interactions
+// create for loop to save values from interactions 
+// query takes util func and determines what the interactions are, return, for loop, doc save to doc
