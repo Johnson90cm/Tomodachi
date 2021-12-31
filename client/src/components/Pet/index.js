51a8c5cd6b1f76879f666c4ebfdc4552c0959eb7
DerVerruckte <michael.heinhold@gmail.com>
2021-12-31 16:50:55 -0700

    got animations to change

--- client/src/components/Pet/index.js
+++ client/src/components/Pet/index.js
@@ -1,11 +1,39 @@
 import React from "react";
 import Calm from '../../assets/alt-calm.gif'
+import Rainfall from "../../assets/flood.gif";
+import Fires from '../../assets/fires.gif';
+import Tornado from '../../assets/tornado.gif'
 import './style.css'
 
-function Pet() {
+function Pet(props) {
+
+    const renderAnimation = () => {
+        switch (props.animation) {
+        case 'rainfall':
+            console.log('cool rainfall animation');
+            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            return Rainfall;
+        case 'sunlight':
+            console.log('cool sunlight animation');
+            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            break;
+        case 'volcanoes':
+            console.log('cool volcanoes animation');
+            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            return Fires;
+        case 'wind':
+            console.log('cool wind animation');
+            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            return Tornado;
+        default:
+            console.log('cool calm animation')
+            return Calm;
+        }
+    }
+
     return (
         <div className="wrapper">
-            <img src={Calm} className="tomodachi" alt="This will display an animated GIF" />
+            <img src={renderAnimation()} className="tomodachi" alt="This will display an animated GIF" />
         </div>
     )
 }
