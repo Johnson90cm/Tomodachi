da3811bc8044c3eddf2f0044c57be9c4cd4cdbc5
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2022-01-03 12:00:57 -0700

    auth.js/signup.js added/refactor

---
+++ client/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Nav from "../components/Nav";
+import Pet from "../components/Pet";
+
+import Contact from "../components/Contact";
+import Support from "../components/Support";
+
+const Home = () => {
+    return (
+      <div className="container">
+        
+        
+        
+      </div>
+    );
+  };
+  
+  export default Home;
