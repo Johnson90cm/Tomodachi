176d87cff7ade677bdd6f38bb90a3625a0f363e3
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2022-01-04 21:11:03 -0700

    uncomment all of Colton's BS. Install client login dependencies

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,14 @@
-// import React from "react";
+import React from "react";
 
-// import Nav from "../components/Nav";
-// import Pet from "../components/Pet";
-// import Interaction from "../components/Interaction";
-// import Contact from "../components/Contact";
-// import Support from "../components/Support";
+import Nav from "../components/Nav";
+import Pet from "../components/Pet";
+import Interaction from "../components/Interaction";
+import Contact from "../components/Contact";
+import Support from "../components/Support";
 
-// // function logic here
+// function logic here
 
-// return (
-//     <div>
-//     </div>
-// )
+return (
+    <div>
+    </div>
+)
