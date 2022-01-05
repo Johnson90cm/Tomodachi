a8604e11ce602689a7b0948e063e402fbc693a2e
Tank <johnson90.cm@gmail.com>
2022-01-04 21:22:21 -0700

    updated animations and timers

--- client/src/components/Pet/index.js
+++ client/src/components/Pet/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Calm from '../../assets/calm.gif'
 import Rainfall from "../../assets/flood.gif";
-import Fires from '../../assets/fires.gif';
+import Volcanos from '../../assets/volcanos.gif';
 import Tornado from '../../assets/tornado.gif'
+import Sunlight from '../../assets/sunlight.gif'
 import './style.css'
 
 function Pet(props) {
@@ -11,19 +12,19 @@ function Pet(props) {
         switch (props.animation) {
         case 'rainfall':
             console.log('cool rainfall animation');
-            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            setTimeout(() => { props.changeAnimation('calm') }, 5000)
             return Rainfall;
         case 'sunlight':
             console.log('cool sunlight animation');
-            setTimeout(() => { props.changeAnimation('calm') }, 2000)
-            break
+            setTimeout(() => { props.changeAnimation('calm') }, 10000)
+            return Sunlight;
         case 'volcanoes':
             console.log('cool volcanoes animation');
-            setTimeout(() => { props.changeAnimation('calm') }, 2000)
-            return Fires;
+            setTimeout(() => { props.changeAnimation('calm') }, 5000)
+            return Volcanos;
         case 'wind':
             console.log('cool wind animation');
-            setTimeout(() => { props.changeAnimation('calm') }, 2000)
+            setTimeout(() => { props.changeAnimation('calm') }, 5000)
             return Tornado;
         default:
             console.log('cool calm animation')
