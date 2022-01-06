9a344041d78dddf5bc79249dd95b95ae4693686a
Taylor Hakes <86327207+SadboiTay@users.noreply.github.com>
2022-01-06 10:28:40 -0700

    move end game logic to Home component

--- client/src/components/Stat/index.js
+++ client/src/components/Stat/index.js
@@ -1,43 +1,12 @@
 import React from 'react';
 
 function Stat(props) {
+    const { statName, currentPlanet } = props
 
-    const { statName, changeAnimation, currentPlanet } = props
-
-    let statNumber;
-
-    const atmosphere = currentPlanet.atmosphere;
-    const biosphere = currentPlanet.biosphere;
-    const hydrosphere = currentPlanet.hydrosphere;
-    const lithosphere = currentPlanet.lithosphere
-    statNumber = currentPlanet[statName]
-
-    //gets a total score to determine percentages
-    const statTotals = atmosphere +
-        biosphere +
-        hydrosphere +
-        lithosphere;
-
-    //checks if planet has reached max age
-    if (currentPlanet.age >= 10000) {
-        //checks to see if the stat is over 35% and the greatest stat to determine the endgame
-        //else has a good endgame
-        if (atmosphere > statTotals * .35 && atmosphere > biosphere && hydrosphere && lithosphere) {
-            changeAnimation('endgame-atmo')
-        } else if (biosphere > statTotals * .35 && biosphere > atmosphere && hydrosphere && lithosphere) {
-            changeAnimation('endgame-bio')
-        } else if (hydrosphere > statTotals * .35 && hydrosphere > biosphere && atmosphere && lithosphere) {
-            changeAnimation('endgame-hydro')
-        } else if (lithosphere > statTotals * .35 && lithosphere > biosphere && hydrosphere && atmosphere) {
-            changeAnimation('endgame-litho')
-        } else {
-            changeAnimation('endgame-good')
-        }
-    }
+    const statNumber = currentPlanet[statName]
 
     return (
         <li className='stat-text'>{statName} {statNumber}</li>
-
     );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,6 +24,35 @@ function Home() {
 
             // set the planet that was created to the current planet for gameplay
             setCurrentPlanet(planet)
+
+            // END GAME LOGIC
+            const atmosphere = currentPlanet.atmosphere;
+            const biosphere = currentPlanet.biosphere;
+            const hydrosphere = currentPlanet.hydrosphere;
+            const lithosphere = currentPlanet.lithosphere
+
+            //gets a total score to determine percentages
+            const statTotals = atmosphere +
+                biosphere +
+                hydrosphere +
+                lithosphere;
+
+            //checks if planet has reached max age
+            if (currentPlanet.age >= 10000) {
+                //checks to see if the stat is over 35% and the greatest stat to determine the endgame
+                //else has a good endgame
+                if (atmosphere > statTotals * .35 && atmosphere > biosphere && hydrosphere && lithosphere) {
+                    changeAnimation('endgame-atmo')
+                } else if (biosphere > statTotals * .35 && biosphere > atmosphere && hydrosphere && lithosphere) {
+                    changeAnimation('endgame-bio')
+                } else if (hydrosphere > statTotals * .35 && hydrosphere > biosphere && atmosphere && lithosphere) {
+                    changeAnimation('endgame-hydro')
+                } else if (lithosphere > statTotals * .35 && lithosphere > biosphere && hydrosphere && atmosphere) {
+                    changeAnimation('endgame-litho')
+                } else {
+                    changeAnimation('endgame-good')
+                }
+            }
         }
     }, [data, currentPlanet]);
 
